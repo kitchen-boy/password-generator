8f6142684149603fde0cdba642212757c5fc3953
kitchen-boy <kaye.chen.boyce@gmail>
2021-11-07 22:24:44 -0500

    Variables for including characters

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,7 @@ var specialCharacters = [
     '+',
     '\\',
     '/',
-    "'",
+    ''',
     '!',
     '#',
     '$',
@@ -92,53 +92,71 @@ var upperCasedCharacters = [
 function getPasswordOptions() {
     // Variable to store length of password from user input
     var length = parseInt(
-        prompt("How many characters would you like your password to contain?")
+        prompt('How many characters would you like your password to contain?')
     );
 
     // Conditional statement to check if password length is a number. //
     // Prompts end if this is false //
     if (isNaN(length) === true) {
-        alert("Password length must be provided as a number");
+        alert('Password length must be provided as a number');
         return;
     }
 
     // Conditional statement to check if password length is a number.//
     // Prompts end if this is false. //
     if (isNaN(length) ===true) {
-        alert("Password length must be provided as a number");
+        alert('Password length must be provided as a number');
         return;
     }
 
     // Conditional statement to check if password length is at least 8 characters long.//
     // Prompts end if this is false. //
     if (length < 8) {
-        alert("Password length must be at least 8 characters");
+        alert('Password length must be at least 8 characters');
         return;
     }
 
     // Conditional statement to check if password length is <128 characters long.//
     //Prompts end if this is false.//
     if (length > 128) {
-        alert("Password length must be less than 129 characters");
+        alert('Password length must be less than 129 characters');
         return;
     }
 
-    //
+    //Variable to store statement re: inclusion of numeric characters
+    var hasNumericCharacters = confirm(
+        'Click OK to confirm including NUMERIC CHARACTERS.'
+    )
+
+    //Variable to store statement re: inclusion of lowercase characters
+    var hasLowerCaseCharacters = confirm(
+        'Click OK to confirm including lowercase CHARACTERS.'
+    )
+
+    //Variable to store statement re: inclusion of UPPERCASE characters
+    var hasUpperCaseCharacters = confirm(
+        'Click OK to confirm including UPPERCASE CHARACTERS.'
+    );
+
+    //Variable to store statement re: inclusion of special characters
+    var hasSpecialCharacters = confirm(
+        'Click OK to confirm including *SPECIAL CHARACTERS*.'
+    );
 
 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
